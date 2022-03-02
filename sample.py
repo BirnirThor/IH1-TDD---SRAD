38dcc4b90b5173d77d6335de9f34a5f72b954e6c
BirnirThor <birnir21@ru.is>
2022-03-02 14:18:44 +0000

    Step 4: Return FizzBuzz if div by 3 and 5

--- sample.py
+++ sample.py
@@ -1,9 +1,12 @@
 
 def fizzbuzz(int):
-    if int % 5 == 0:
+    if int % 5 == 0 and int % 3 == 0:
+        return "FizzBuzz"
+        
+    elif int % 5 == 0:
         return "Buzz"
        
     elif int % 3 == 0:
         return "Fizz"
-        
+
     return int
--- test_sample.py
+++ test_sample.py
@@ -12,11 +12,20 @@ def test2_return_buzz_if_div_by_5():
 def test3_return_buzz_if_div_by_5():
     assert fizzbuzz(115) == "Buzz"
 
-def test1_return_buzz_if_div_by_3():
+def test1_return_fizz_if_div_by_3():
     assert fizzbuzz(3) == "Fizz"
 
-def test2_return_buzz_if_div_by_3():
+def test2_return_fizz_if_div_by_3():
     assert fizzbuzz(-9) == "Fizz"
 
-def test3_return_buzz_if_div_by_3():
+def test3_return_fizz_if_div_by_3():
     assert fizzbuzz(102) == "Fizz"
+
+def test1_return_fizzbuzz_if_div_by_both():
+    assert fizzbuzz(15) == "FizzBuzz"
+
+def test2_return_fizzbuzz_if_div_by_both():
+    assert fizzbuzz(30) == "FizzBuzz"
+
+def test3_return_fizzbuzz_if_div_by_both():
+    assert fizzbuzz(330) == "FizzBuzz"
