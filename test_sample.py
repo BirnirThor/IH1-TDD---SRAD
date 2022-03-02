c891579ca4dc3cf9ccf97d749c25be6d4793a3b6
BirnirThor <birnir21@ru.is>
2022-03-02 14:11:23 +0000

    Step 2: More tests for div by 5

--- test_sample.py
+++ test_sample.py
@@ -3,5 +3,11 @@ from sample import fizzbuzz
 def test_return_the_input_num():
     assert fizzbuzz(16) == 16
 
-def test_return_buzz_if_div_by_5():
+def test1_return_buzz_if_div_by_5():
     assert fizzbuzz(5) == "Buzz"
+
+def test2_return_buzz_if_div_by_5():
+    assert fizzbuzz(-5) == "Buzz"
+
+def test3_return_buzz_if_div_by_5():
+    assert fizzbuzz(115) == "Buzz"
