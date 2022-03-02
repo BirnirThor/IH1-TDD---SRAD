8f182530a3f88d7c5e67805eb0ca8e9ff7c97235
BirnirThor <birnir21@ru.is>
2022-03-02 14:08:16 +0000

    Step 2: Buzz if input is div by 5

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
 
 def fizzbuzz(int):
+    if int % 5 == 0:
+        return "Buzz"
     return int
--- test_sample.py
+++ test_sample.py
@@ -3,4 +3,5 @@ from sample import fizzbuzz
 def test_return_the_input_num():
     assert fizzbuzz(16) == 16
 
-
+def test_return_buzz_if_div_by_5():
+    assert fizzbuzz(5) == "Buzz"
